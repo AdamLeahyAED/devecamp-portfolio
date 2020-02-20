c9a162e0258904b461fa7ebb5471aa2053edc74f
Adam <adam@aedglobal.com.au>
2020-02-20 14:27:36 +1000

    created seeds file for data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "fake data ..."
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
+    like Aldus PageMaker including versions of Lorem Ipsum."
   )
 end
 
@@ -20,7 +26,9 @@ puts "5 skills created"
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "My great service",
-    body: "fake data ...",
+    body: "It is a long established fact that a reader will be distracted by the readable 
+    content of a page when looking at its layout. The point of using Lorem Ipsum is that it 
+    has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here",
     main_image: "https://place-hold.it/600x400",
     thumb_image: "https://place-hold.it/350x200"
   )
